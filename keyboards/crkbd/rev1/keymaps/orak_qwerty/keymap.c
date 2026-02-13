0e25137db5c9ae1c479f839112d014b669c7195d
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-02-13 22:18:17 +0100

    orak_qwerty: improved :)

--- keyboards/crkbd/rev1/keymaps/orak_qwerty/keymap.c
+++ keyboards/crkbd/rev1/keymaps/orak_qwerty/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, KC_Q, KC_W, KC_E, KC_R,  KC_T,                                     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,  KC_A ,   KC_S ,   KC_D ,   KC_F , KC_G,                           KC_H,     KC_J ,    KC_K ,     KC_L ,  KC_SEMICOLON,   XXXXXXX,
+     KC_ESC,  KC_A ,   KC_S ,   KC_D ,   KC_F , KC_G,                           KC_H,     KC_J ,    KC_K ,     KC_L ,  KC_SEMICOLON,   KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
    XXXXXXX, KC_Z,  KC_X,  KC_C,    KC_V, KC_B,                           KC_N,    KC_M,    KC_COMMA,    KC_DOT,     KC_QUOTE,  XXXXXXX,
  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -65,11 +65,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
+
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX, KC_GRV, KC_LBRC, KC_LCBR, KC_LPRN, KC_PLUS,                     KC_MINUS, KC_RPRN, KC_RCBR, KC_RBRC, KC_ESC,    XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXX,  OS_CMD,  OS_ALT,  OS_SHFT,  OS_CTRL, KC_ENT,                    KC_BSPC, KC_DLR, KC_HASH,  KC_EQL, KC_PERC,  XXXXXXX,
+       KC_ESC,  OS_CMD,  OS_ALT,  OS_SHFT,  OS_CTRL, KC_ENT,                    KC_BSPC, KC_DLR, KC_HASH,  KC_EQL, KC_PERC,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        XXXXXXX, KC_TILD,  KC_AT,  KC_BACKSLASH, KC_EXLM,  KC_SLASH,              KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_QUES,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -77,11 +78,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
+
     [2] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    XXXXXXX,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                       LSFT(KC_QUOTE), C(KC_A), C(KC_C), A(KC_X), KC_ESC, XXXXXXX,
+  //,---------------------------------------------------.                    ,-----------------------------------------------------.
+    XXXXXXX,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                       LSFT(KC_QUOTE), C(KC_Q), C(KC_C), C(KC_L), KC_ESC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    XXXXXXX,  KC_6,    KC_7,     KC_8,    KC_9,    KC_0,                        KC_BSPC,  OS_CTRL , OS_SHFT, OS_ALT, OS_CMD, XXXXXXX,
+    KC_ESC,  KC_6,    KC_7,     KC_8,    KC_9,    KC_0,                        KC_BSPC,  OS_CTRL , OS_SHFT, OS_ALT, OS_CMD, KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_TILD,  KC_AT,  KC_BACKSLASH, KC_EXLM,  KC_SLASH,              KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_QUES,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -93,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      XXXXXXX, KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,                      KC_F11,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     XXXXXXX, OS_CMD,   OS_ALT, OS_SHFT, OS_CTRL, KC_PSCR,                      KC_F12,  KC_LEFT,  KC_DOWN,  KC_UP, KC_RIGHT, XXXXXXX,
+     KC_ESC, OS_CMD,   OS_ALT, OS_SHFT, OS_CTRL, KC_PSCR,                      KC_F12,  KC_LEFT,  KC_DOWN,  KC_UP, KC_RIGHT, KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                      KC_MPLY, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
